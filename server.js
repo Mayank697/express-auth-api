52a6393f69b7f338e882d4aa4444b6c7ec6faabc
Mayank Singh <smayank2000@gmail.com>
2021-06-25 19:01:53 +0530

    frontend rendering done

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('_middleware/error-handler');
+const path = require('path');
+const routes = require('./routes/index');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 // api routes
 app.use('/users', require('./users/users.controller'));
+
+// routes
+app.use('/', routes);
+
 
 // global error handler
 app.use(errorHandler);
