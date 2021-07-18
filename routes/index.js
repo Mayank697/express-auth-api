0c5955137e730a9d5d6c03ea6c72db8103671062
Mayank Singh <smayank2000@gmail.com>
2021-07-18 08:24:10 +0530

    confilts with pankaj2 branch resolved

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const config = require('config.json'); ////changed =======================
-const jwt = require('jsonwebtoken'); ////changed ======================
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
     if (req.cookies.auth) {
@@ -24,61 +24,13 @@ router.get('/dashboard', (req, res) => {
     // console.log("cookie is the game",req.cookies.auth);
     if (req.cookies.auth) {
         jwt.verify(req.cookies.auth.token, config.secret, function(err, decoded) {
-            // console.log("decoded bhai yhai hai culprit", req.cookies.auth)
             if (!err) {
                 data = {
                     username: req.cookies.auth.firstName,
                     email: req.cookies.auth.username
                 };
-                // readProduct(function (products) {
-                //     readCart(function (cart) {
-                //         res.render("dashboard", { data: data, cart_items: cart })
-                //     })
-                // })
-                records = [{
-                        id: 1,
-                        name: "Rich-Boss India Pvt Ltd. - Chirya",
-                        address: "Jaipur",
-                        city: "Chirya",
-                        state: "Haryana"
-                    },
-                    {
-                        id: 2,
-                        name: "Rich-Boss A&C India Pvt.Ltd. - Surat",
-                        address: "Surat",
-                        city: "Surat",
-                        state: "Gujarat"
-                    },
-                    {
-                        id: 3,
-                        name: "Rich-Boss Cement Plant -Greater Noida",
-                        address: "Kanpur",
-                        city: "Kanpur",
-                        state: "Uttar Pradesh"
-                    },
-                    {
-                        id: 4,
-                        name: "Rich-Boss ACC Limited -Chandrapur",
-                        address: "Chandrapur",
-                        city: "Chandrapur",
-                        state: "Maharashtra"
-                    },
-                    {
-                        id: 5,
-                        name: "Rich-Boss Cement Plant - Raseda",
-                        address: "Raseda",
-                        city: "Raseda",
-                        state: "Chhattisgarh"
-                    },
-                    {
-                        id: 6,
-                        name: "Rich-Boss Cement Factory- Lakheri",
-                        address: "Lakheri",
-                        city: "Lakheri",
-                        state: "Rajasthan"
-                    }
-                ]
-                res.render("dashboard", { records: records, data: data })
+                let token = req.cookies.auth.token;
+                res.render("dashboard", { data: data, token: token })
             } else {
                 res.redirect("/");
             }
